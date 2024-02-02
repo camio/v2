306a8cb4302708fc39272b2f7e7b84e3386e2c95
David Sankel <dsankel@adobe.com>
2024-02-02 18:44:00 -0500

    initial

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((org-mode . ((org-html-preamble .
+                                 "<h1 class=\"title\">%t</h1>
+                   <p class=\"subtitle\">
+                      Date: %d<br>
+                      Author: %a<br>
+                      Audience: Evolution Working Group<br>
+                      Paper: TBD
+                   </p>")
+              )))
